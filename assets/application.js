a3578064190558429bc72c8ab5687b627609d06a
Ajdin Imsirovic <ajdika@live.com>
2021-07-13 07:42:54 +0200

    [TASK] Improve the sorting JS code to avoid errors on other pages

--- assets/application.js
+++ assets/application.js
@@ -1,8 +1,10 @@
 // Put your application javascript here
-// alert('It works!');
-document.querySelector('#sort_by').addEventListener('change', function(e) {
-    let url = new URL(window.location.href);
-    url.searchParams.set('sort_by', e.currentTarget.value);
 
-    window.location = url.href;
-})
+if (document.getElementById('sort_by') != null) {
+    document.querySelector('#sort_by').addEventListener('change', function(e) {
+        let url = new URL(window.location.href);
+        url.searchParams.set('sort_by', e.currentTarget.value);
+
+        window.location = url.href;
+    });
+}
