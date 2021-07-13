e8ad4dad13d9a9cfb434c36d40a7d32c630a48c5
Ajdin Imsirovic <ajdika@live.com>
2021-07-13 07:30:42 +0200

    [TASK] Add sorting on product collections all page

--- assets/application.js
+++ assets/application.js
@@ -1,2 +1,8 @@
 // Put your application javascript here
 // alert('It works!');
+document.querySelector('#sort_by').addEventListener('change', function(e) {
+    let url = new URL(window.location.href);
+    url.searchParams.set('sort_by', e.currentTarget.value);
+
+    window.location = url.href;
+})
