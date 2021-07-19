7f7c46641c692be7927637725f4ed2a282102fd3
Ajdin Imsirovic <ajdika@live.com>
2021-07-19 12:20:54 +0200

    [TASK] Account activation and reset pw

--- assets/application.js
+++ assets/application.js
@@ -30,3 +30,13 @@ if(document.getElementById('AddressCountryNew') != null) {
         provinceSelector.innerHTML = options;
     })
 }
+
+if(document.getElementById('forgotPassword') != null) {
+    document.getElementById('forgotPassword').addEventListener('click', function(event) {
+        const element = document.querySelector('#forgot_password_form');
+        if(element.classList.contains('d-none')) {
+            element.classList.remove('d-none');
+            element.classList.add('d-block');
+        }
+    });
+}
