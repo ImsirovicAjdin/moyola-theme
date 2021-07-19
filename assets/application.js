f9882f5925572c44a5a0bf74d07333a57473911c
Ajdin Imsirovic <ajdika@live.com>
2021-07-19 15:18:24 +0200

    [TASK] Add product modal popups

--- assets/application.js
+++ assets/application.js
@@ -50,3 +50,26 @@ if (localeItems.length > 0) {
         })
     });
 }
+
+let productInfoAnchors = document.querySelectorAll('#productInfoAnchor');
+let productModal = new bootstrap.Modal(document.getElementById('productInfoModal', {}));
+if(productInfoAnchors.length > 0) {
+    productInfoAnchors.forEach(item => {
+        item.addEventListener('click', event => {
+
+            let url = '/products/' + item.getAttribute('product-handle') + '.js';
+            fetch(url)
+            .then(resp => resp.json())
+            .then(data => {
+                console.log(data);
+
+                document.getElementById('productInfoImg').src = data.images[0];
+                document.getElementById('productInfoTitle').innerHTML = data.title;
+                document.getElementById('productInfoPrice').innerHTML = item.getAttribute('product-price');
+                document.getElementById('productInfoDescription').innerHTML = data.description;
+
+                productModal.show();
+            });
+        });
+    });
+};
